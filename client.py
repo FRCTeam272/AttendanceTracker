0279c5418f9cd75341a4e414eca9dea9b22effa6
Jake Gadaleta <me@jakegads.dev>
2024-09-07 15:37:47 -0400

    added some logging, added backup handling

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import requests
-
+import os
 def generate_end_point(student_id, event_id, reporter):
     return f"http://127.0.0.1:5000/add/attendance?student_id={student_id}&event_id={event_id}&reporter={reporter}"
 
@@ -13,15 +13,33 @@ def read_scanner_input():
             print(f"Endpoint: {endpoint}")
             try:
                 response = requests.post(endpoint)
+                if response.status_code == 200 or response.status_code == 201:
+                    print("Success")
             except:
                 print("Error: Could not connect to server, saving to a file")
                 with open("backup.dat", "a+") as f:
                     f.write(f"{endpoint}\n")
             requests.post(endpoint)
         
+def try_to_sync():
+    try:
+        with open("backup.dat", "r+") as f:
+            for line in f:
+                try:
+                    requests.post(line)
+                except:
+                    print("Error: Could not connect to server, will continue to write to file")
+                    return
+        print("All backups synced")
+        os.remove("backup.dat")
+    except:
+        print("No backups found")    
+    
+
         
 if __name__ == "__main__":
     try:
+        try_to_sync()
         read_scanner_input()
     except KeyboardInterrupt:
         print("Exiting...")
