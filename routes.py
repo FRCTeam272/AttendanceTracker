0ab3fe6c336c1bfa60f97762bf87ae4a1353266d
Jake Gadaleta <me@jakegads.dev>
2024-09-20 18:23:01 -0400

    reworked the procfile again

--- routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import server_functions as sf
-
-
-if __name__ == '__main__':
-    import uvicorn
-    import webbrowser
-    webbrowser.open('http://127.0.0.1:8000/docs')
-    uvicorn.run(app, host="127.0.0.1", port=8000) # reload is true here for testing, will be solid on deployment
